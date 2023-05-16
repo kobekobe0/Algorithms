e796cda943422a37a3bfcc32f99e583cf6878695
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2023-05-16 20:46:27 +0800

    Finished

---
+++ src/main/java/com/mycompany/algorithms/Algorithms.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algorithms;
+
+/**
+ *
+ * @author kobe0
+ */
+public class Algorithms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
